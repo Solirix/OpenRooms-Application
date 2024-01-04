85b85ae353ddc86271bd8bba95fd0dcb713cd851
rome6315 <romo6315@gmail.com>
2024-01-04 10:11:33 -0500

    add icons to tab bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,50 @@
 import 'package:flutter/cupertino.dart';
+// Cupertino is iOS styling. Look up flutter cupertino there's a lot of docs.
+/// Flutter code sample for [CupertinoTabBar].
 
-/// Flutter code sample for [CupertinoTabScaffold].
+void main() => runApp(const CupertinoTabBarApp());
 
-void main() => runApp(const TabScaffoldApp());
-
-class TabScaffoldApp extends StatelessWidget {
-  const TabScaffoldApp({super.key});
+class CupertinoTabBarApp extends StatelessWidget {
+  const CupertinoTabBarApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const CupertinoApp(
+      //can change this to dark for dark mode it looks like
       theme: CupertinoThemeData(brightness: Brightness.light),
-      home: TabScaffoldExample(),
+      home: CupertinoTabBarExample(),
     );
   }
 }
 
-class TabScaffoldExample extends StatefulWidget {
-  const TabScaffoldExample({super.key});
+class CupertinoTabBarExample extends StatelessWidget {
+  const CupertinoTabBarExample({super.key});
 
-  @override
-  State<TabScaffoldExample> createState() => _TabScaffoldExampleState();
-}
-
-class _TabScaffoldExampleState extends State<TabScaffoldExample> {
   @override
   Widget build(BuildContext context) {
     return CupertinoTabScaffold(
+      //create the tab bar
       tabBar: CupertinoTabBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.home),
-            label: 'Home',
+            icon: Icon(CupertinoIcons.map_fill),
+            //label: 'Map',
           ),
           BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.search_circle_fill),
-            label: 'Explore',
+            icon: Icon(CupertinoIcons.house_fill),
+            //label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings_solid),
+            //label: 'Settings',
           ),
         ],
       ),
       tabBuilder: (BuildContext context, int index) {
         return CupertinoTabView(
           builder: (BuildContext context) {
-            return CupertinoPageScaffold(
-              navigationBar: CupertinoNavigationBar(
-                middle: Text('Page 1 of tab $index'),
-              ),
-              child: Center(
-                child: CupertinoButton(
-                  child: const Text('Next page'),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      CupertinoPageRoute<void>(
-                        builder: (BuildContext context) {
-                          return CupertinoPageScaffold(
-                            navigationBar: CupertinoNavigationBar(
-                              middle: Text('Page 2 of tab $index'),
-                            ),
-                            child: Center(
-                              child: CupertinoButton(
-                                child: const Text('Back'),
-                                onPressed: () {
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  },
-                ),
-              ),
+            return Center(
+              child: Text('Content of tab $index'),
             );
           },
         );
