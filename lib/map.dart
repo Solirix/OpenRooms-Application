b4f6f52ced9299ac73862e12cab1e1b30e9b7477
Cole McNeil <csmcneil93@comcast.net>
2024-02-04 19:09:48 -0500

    Adding static Map with fixed overlay.

--- lib/map.dart
+++ lib/map.dart
@@ -19,75 +19,77 @@ class MapPage extends StatelessWidget {
               top: 5,
               bottom: 5,
               child: Image.asset(
-                'lib/assets/images/map1.png',
+                //'lib/assets/images/map1.png',
+                'lib/assets/images/mapF1.png',
+                //fit: BoxFit.fill,
               ),
             ),
 
             // Specific room overlays
             // Replace with actual data and positions
-            Positioned(
-              left: 122, // Example X position
-              top: 83, // Example Y position
-              child: Image.asset(
-                _getRoomStatusImage1('Available'),
-                width: 120,
-              ),
-            ),
-            Positioned(
-              left: 369, // Example X position
-              top: 83, // Example Y position
-              child: Image.asset(
-                _getRoomStatusImage2('Offline'),
-                width: 120,
-              ),
-            ),
-            Positioned(
-              left: 369, // Example X position
-              top: 610, // Example Y position
-              child: Image.asset(
-                _getRoomStatusImage3('Not Available'),
-                width: 120,
-              ),
-            ),
+//            Positioned(
+//              left: 122, // Example X position
+//              top: 83, // Example Y position
+//              child: Image.asset(
+//                _getRoomStatusImage1('Available'),
+//                width: 120,
+//              ),
+//            ),
+//            Positioned(
+//              left: 369, // Example X position
+//              top: 83, // Example Y position
+//              child: Image.asset(
+//                _getRoomStatusImage2('Offline'),
+//                width: 120,
+//              ),
+//            ),
+//            Positioned(
+//              left: 369, // Example X position
+//              top: 610, // Example Y position
+//              child: Image.asset(
+//                _getRoomStatusImage3('Not Available'),
+//                width: 120,
+//              ),
+//             ),
           ],
         ),
       ),
     );
   }
 
-  String _getRoomStatusImage1(String status) {
-    switch (status) {
-      case 'Available':
-        return 'lib/assets/images/available1.png';
-      case 'Offline':
-        return 'lib/assets/images/offline1.png';
-      case 'Not Available':
-      default:
-        return 'lib/assets/images/not_available1.png';
-    }
-  }
+  // String _getRoomStatusImage1(String status) {
+  //   switch (status) {
+  //     case 'Available':
+  //       return 'lib/assets/images/available1.png';
+  //     case 'Offline':
+  //       return 'lib/assets/images/offline1.png';
+  //     case 'Not Available':
+  //     default:
+  //       return 'lib/assets/images/not_available1.png';
+  //   }
+  // }
 
-  String _getRoomStatusImage2(String status) {
-    switch (status) {
-      case 'Available':
-        return 'lib/assets/images/available2.png';
-      case 'Offline':
-        return 'lib/assets/images/offline2.png';
-      case 'Not Available':
-      default:
-        return 'lib/assets/images/not_available2.png';
-    }
-  }
+  // String _getRoomStatusImage2(String status) {
+  //   switch (status) {
+  //     case 'Available':
+  //       return 'lib/assets/images/available2.png';
+  //     case 'Offline':
+  //       return 'lib/assets/images/offline2.png';
+  //     case 'Not Available':
+  //     default:
+  //       return 'lib/assets/images/not_available2.png';
+  //   }
+  // }
 
-  String _getRoomStatusImage3(String status) {
-    switch (status) {
-      case 'Available':
-        return 'lib/assets/images/available3.png';
-      case 'Offline':
-        return 'lib/assets/images/offline3.png';
-      case 'Not Available':
-      default:
-        return 'lib/assets/images/not_available3.png';
-    }
-  }
+  // String _getRoomStatusImage3(String status) {
+  //   switch (status) {
+  //     case 'Available':
+  //       return 'lib/assets/images/available3.png';
+  //     case 'Offline':
+  //       return 'lib/assets/images/offline3.png';
+  //     case 'Not Available':
+  //     default:
+  //       return 'lib/assets/images/not_available3.png';
+  //   }
+  // }
 }
