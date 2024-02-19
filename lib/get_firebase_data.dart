04d2a9e1e31e1babc0cd26038fb2965984a284e9
rome6315 <romo6315@gmail.com>
2024-02-19 14:00:28 -0500

    read in hourly data to app

--- lib/get_firebase_data.dart
+++ lib/get_firebase_data.dart
@@ -12,7 +12,36 @@ class FirebaseRoomService {
     DatabaseReference roomRef = firebaseDatabase.ref().child(roomId);
     return roomRef.onValue.map((event) => event.snapshot.value.toString());
   }
+
+  Stream<Map<int, String>> getOccupancyDataForDateAndRoom(
+      String date, String roomId) {
+    DatabaseReference dateRoomRef =
+        firebaseDatabase.ref().child('occupancy').child(date).child(roomId);
+    return dateRoomRef.onValue.map((event) {
+      // The value can be a Map or a List
+      final value = event.snapshot.value;
+      Map<int, String> hourData = {};
+      if (value is Map) {
+        value.forEach((key, val) {
+          final hour = int.tryParse(key.toString());
+          if (hour != null) {
+            hourData[hour] = val.toString();
+          }
+        });
+      } else if (value is List) {
+        // Convert List to Map
+        for (int i = 0; i < value.length; i++) {
+          if (value[i] != null) {
+            hourData[i] = value[i].toString();
+          }
+        }
+      }
+      return hourData;
+    });
+  }
 }
+
+
 // import 'dart:async';
 // import 'package:firebase_database/firebase_database.dart';
 
--- lib/hourly_occupancy.dart
+++ lib/hourly_occupancy.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart'; //needed to customize the date picker
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
-//import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:openrooms/get_firebase_data.dart';
 
 // class HourlyOccupancy extends StatefulWidget {
 //   const HourlyOccupancy({super.key});
@@ -107,7 +108,7 @@ import 'package:flutter_heatmap_calendar/flutter_heatmap_calendar.dart';
 // }
 
 class HourlyOccupancy extends StatefulWidget {
-  const HourlyOccupancy({Key? key}) : super(key: key);
+  const HourlyOccupancy({super.key});
 
   @override
   State<HourlyOccupancy> createState() => _HourlyOccupancyState();
@@ -115,44 +116,178 @@ class HourlyOccupancy extends StatefulWidget {
 
 class _HourlyOccupancyState extends State<HourlyOccupancy> {
   DateTime? selectedDate = DateTime.now();
+  CalendarFormat calendarFormat = CalendarFormat.month;
   final TextEditingController _calendarController = TextEditingController();
   bool showCalendar = false; // Added variable to control calendar visibility
+  Map<int, String>? occupancyData; // Variable to hold occupancy data
+
+  @override
+  void initState() {
+    super.initState();
+    fetchOccupancyData(); // Fetch initial data for the current date
+  }
+
+  void fetchOccupancyData() {
+    String formattedDate = DateFormat('yyyy-MM-dd').format(selectedDate!);
+    String roomId = "room1"; // Adjust the room ID as needed
+    FirebaseRoomService firebaseRoomService =
+        FirebaseRoomService(firebaseDatabase: FirebaseDatabase.instance);
+
+    firebaseRoomService
+        .getOccupancyDataForDateAndRoom(formattedDate, roomId)
+        .listen((hourData) {
+      // Now we directly receive Map<int, String>
+      setState(() {
+        occupancyData = hourData;
+      });
+    }, onError: (error) {
+      print("Error fetching occupancy data: $error");
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
       child: CupertinoPageScaffold(
         navigationBar: const CupertinoNavigationBar(
-          middle: Text('Hourly Occupancy'),
+          middle: Text('Hourly Occupancy', style: TextStyle(fontSize: 30)),
         ),
         child: SafeArea(
-          child: HeatMapCalendar(
-            defaultColor: Colors.white,
-            flexible: true,
-            colorMode: ColorMode.color,
-            datasets: {
-              DateTime(2021, 1, 6): 3,
-              DateTime(2021, 1, 7): 7,
-              DateTime(2021, 1, 8): 10,
-              DateTime(2021, 1, 9): 13,
-              DateTime(2021, 1, 13): 6,
-            },
-            colorsets: const {
-              1: Colors.red,
-              3: Colors.orange,
-              5: Colors.yellow,
-              7: Colors.green,
-              9: Colors.blue,
-              11: Colors.indigo,
-              13: Colors.purple,
-            },
-            onClick: (value) {
-              ScaffoldMessenger.of(context)
-                  .showSnackBar(SnackBar(content: Text(value.toString())));
-            },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Data for: ",
+                      style: TextStyle(
+                        color: CupertinoColors.label.resolveFrom(context),
+                        fontSize: 20,
+                      ),
+                    ),
+                    Text(
+                      selectedDate != null
+                          ? DateFormat.yMMMd().format(selectedDate!)
+                          : "Select a date",
+                      style: TextStyle(
+                        color: CupertinoColors.label.resolveFrom(context),
+                        fontSize: 20,
+                      ),
+                    ),
+                    CupertinoButton(
+                      padding: const EdgeInsets.only(left: 10),
+                      onPressed: () {
+                        setState(() {
+                          showCalendar = !showCalendar;
+                        });
+                      },
+                      child: const Icon(CupertinoIcons.calendar),
+                    ),
+                  ],
+                ),
+                if (showCalendar)
+                  TableCalendar(
+                    calendarFormat: calendarFormat,
+                    selectedDayPredicate: (day) {
+                      return isSameDay(selectedDate, day);
+                    },
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        selectedDate = selectedDay;
+                        showCalendar =
+                            false; // Hide calendar after a date is selected
+                        fetchOccupancyData(); // Fetch occupancy data for the new selected date
+                      });
+                    },
+                    focusedDay: selectedDate ?? DateTime.now(),
+                    firstDay: DateTime.utc(2020, 1, 1),
+                    lastDay: DateTime.utc(2030, 12, 31),
+                    calendarStyle: CalendarStyle(
+                      selectedDecoration: const BoxDecoration(
+                        color: CupertinoColors.activeBlue,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                      titleTextStyle: TextStyle(
+                        color: CupertinoColors.label.resolveFrom(context),
+                      ),
+                    ),
+                  ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: occupancyData?.length ?? 0,
+                    itemBuilder: (context, index) {
+                      String hour =
+                          occupancyData!.keys.elementAt(index).toString();
+                      String status = occupancyData!.values.elementAt(index);
+                      return ListTile(
+                        title: Text("Hour $hour: $status"),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
 }
+
+
+// class HourlyOccupancy extends StatefulWidget {
+//   const HourlyOccupancy({Key? key}) : super(key: key);
+
+//   @override
+//   State<HourlyOccupancy> createState() => _HourlyOccupancyState();
+// }
+
+// class _HourlyOccupancyState extends State<HourlyOccupancy> {
+//   DateTime? selectedDate = DateTime.now();
+//   final TextEditingController _calendarController = TextEditingController();
+//   bool showCalendar = false; // Added variable to control calendar visibility
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Material(
+//       child: CupertinoPageScaffold(
+//         navigationBar: const CupertinoNavigationBar(
+//           middle: Text('Hourly Occupancy'),
+//         ),
+//         child: SafeArea(
+//           child: HeatMapCalendar(
+//             defaultColor: Colors.white,
+//             flexible: true,
+//             colorMode: ColorMode.color,
+//             datasets: {
+//               DateTime(2021, 1, 6): 3,
+//               DateTime(2021, 1, 7): 7,
+//               DateTime(2021, 1, 8): 10,
+//               DateTime(2021, 1, 9): 13,
+//               DateTime(2021, 1, 13): 6,
+//             },
+//             colorsets: const {
+//               1: Colors.red,
+//               3: Colors.orange,
+//               5: Colors.yellow,
+//               7: Colors.green,
+//               9: Colors.blue,
+//               11: Colors.indigo,
+//               13: Colors.purple,
+//             },
+//             onClick: (value) {
+//               ScaffoldMessenger.of(context)
+//                   .showSnackBar(SnackBar(content: Text(value.toString())));
+//             },
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
