8754156520a0b749e4e87871ae0627ab7aa1ca60
Pine-1 <droskeadam@gmail.com>
2024-02-21 17:19:14 -0500

    Fixed issues for Code review

--- test/home/room_click_paths.dart
+++ test/home/room_click_paths.dart
@@ -5,15 +5,29 @@ import 'package:openrooms/hourly_occupancy.dart';
 import 'package:mockito/mockito.dart';
 import 'package:openrooms/get_firebase_data.dart';
 
+/// This file is dedicated to testing navigation interactions with tapping on room options from the HomePage to navigate to 
+/// the HourlyOccupancy page and then using the back button to return. It leverages the `flutter_test` framework for widget testing and 
+/// `mockito` for mocking the FirebaseRoomService, allowing for testing of UI flow and service interactions without actual network calls.
+///
+/// Key Features Tested:
+/// - Navigation to HourlyOccupancy page upon selecting an available room.
+/// - Returning to the HomePage when the back button is pressed.
+/// - Ensuring the HourlyOccupancy page is not displayed for offline rooms.
+
+
 class MockFirebaseRoomService extends Mock implements FirebaseRoomService {
   @override
   Stream<String> getRoomValueStream(String roomId) {
+    // Override the getRoomValueStream method to provide a mock response.
+    // This returns a stream that emits a single 'null' string, simulating
+    // the behavior of fetching data for a specific room from Firebase without making real network requests.
     return super.noSuchMethod(Invocation.method(#getRoomValueStream, [roomId]),
         returnValue: Stream.fromIterable(['null']),
         returnValueForMissingStub: Stream.fromIterable(['null']));
   }
 }
 
+//Begin Test to navigate between HourlyOccupancy page and back to Home Page
 void main() {
   testWidgets('Tap on the back button', (tester) async {
     // Create a mock FirebaseRoomService
