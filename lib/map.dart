c8413647edc20c46c45c380a92cab80d6f2db25c
Pine-1 <droskeadam@gmail.com>
2024-02-21 16:39:05 -0500

    Fixed issue for code review

--- lib/map.dart
+++ lib/map.dart
@@ -2,6 +2,16 @@ import 'package:flutter/cupertino.dart';
 import 'dart:async';
 import 'package:openrooms/get_firebase_data.dart';
 
+/// This file defines the `MapPage` StatefulWidget, which is responsible for displaying
+/// a dynamic map image indicating the status of various rooms managed by the `FirebaseRoomService`.
+/// The map updates in real-time to reflect the availability status (available, unavailable, or offline)
+/// of each room based on data streamed from Firebase.
+///
+/// Features:
+/// - Real-time room status updates: Listens to changes in room availability and updates the UI accordingly.
+/// - Dynamic map rendering: Adjusts the displayed map image based on the current room statuses.
+/// - Resource management: Properly subscribes to and unsubscribes from Firebase streams to manage app resources efficiently.
+
 class MapPage extends StatefulWidget {
   final FirebaseRoomService firebaseRoomService;
 
